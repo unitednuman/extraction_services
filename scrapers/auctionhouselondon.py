3d53c6652016a0b3283a9249ce787e540e9a4720
bhatti013 <wahabbhatti22@gmail.com>
2022-05-17 23:01:10 +0500

    auctionhouselondon scraper

---
+++ scrapers/auctionhouselondon.py
@@ -0,0 +1,74 @@
+import requests
+
+class AuctionHouseLondon:
+    DOMAIN = 'https://auctionhouselondon.co.uk/'
+    URL = 'https://auctionhouselondon.co.uk/page-data/future-auctions/page-data.json'
+
+    def __init__(self):
+        pass
+
+    def connect_to(self, url, headers={}, payload={}):
+        print(url)
+        re = requests.get(url, headers=headers, data=payload)
+        print(f"------ Request Response : {re.status_code} --------")
+        return re
+
+    def prepare_price(self, price):
+        try:
+           price = float(price.split('-')[0].replace('£', '').replace('+', '').replace(',', ''))
+        except Exception as ex:
+            print(f"Exception in Price Function - {ex}")
+        return price
+
+    def parser(self, data):
+        data_array = []
+        for json_data in data['result']['pageContext']['auctions']:
+            for lot_details in json_data['Lots']:
+                if not lot_details:
+                    pass
+                price = self.prepare_price(lot_details['GuidePrice'])
+                full_address = lot_details['FullAddress']
+                url_id = "-".join(full_address.replace(',','').split())
+                lot_id = lot_details['ID']
+                url = f"https://auctionhouselondon.co.uk/page-data/lot/{url_id}-{lot_id}/page-data.json".lower()
+                try:
+                    inner_res = self.connect_to(url)
+                except Exception as ex:
+                    print(f"URL issue {ex}")
+
+                inner_details = inner_res.json()['result']['pageContext']
+                auction_date = inner_details['AuctionDate']
+                auction_hours = auction_date.split('T')[1]
+                auc_hours = ":".join([auction_hours.split(":")[0], auction_hours.split(":")[1]])
+                data_hash = {
+                    "_id": lot_id,
+                    "guidePrice": price,
+                    "pictureLink": lot_details['Thumbnail'],
+                    "propertyDescription": inner_details['Description'],
+                    "propertyLink": url.replace('page-data.json','').replace('/page-data',''),
+                    "address": full_address,
+                    "postcode": lot_details['PostCode'],
+                    "numberOfBedrooms": inner_details['Bedrooms'],
+                    "propertyType": inner_details['PropertyType'],
+                    "tenure": inner_details['TenureType'],
+                    "auctionDetails": {
+                        "date": auction_date,
+                        "hour": auc_hours,
+                        "venue": None,
+                    },
+                    "domain": self.DOMAIN
+                }
+                data_array.append(data_hash)
+        return data_array
+
+    def scraper(self):
+        response = self.connect_to(self.URL)
+        data = response.json()
+        item = []
+        item = self.parser(data)
+
+
+try:
+    AuctionHouseLondon().scraper()
+except Exception as e:
+    print(e)
