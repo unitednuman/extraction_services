9c55f703016c28b906210504ad03748686267a80
Numan Aslam <unitednuman@gmail.com>
2022-05-19 20:37:34 +0500

    data

--- deploy/dev/django/entrypoint.sh
+++ deploy/dev/django/entrypoint.sh
@@ -7,4 +7,4 @@ set -o nounset
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 
-gunicorn nfc.wsgi:application -w 2 -b :8000 --timeout 120 --graceful-timeout 120 --worker-class gevent
+gunicorn services.wsgi:application -w 2 -b :8000 --timeout 120 --graceful-timeout 120 --worker-class gevent
