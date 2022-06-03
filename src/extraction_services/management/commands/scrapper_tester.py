1da472dd7389beb4a7e2407615fe0189f8bc8794
hassan junaid <hassanj225@gmail.com>
2022-06-03 18:55:51 +0500

    added new scrapper for savills

--- src/extraction_services/management/commands/scrapper_tester.py
+++ src/extraction_services/management/commands/scrapper_tester.py
@@ -2,8 +2,6 @@ import importlib
 from django.core.management.base import BaseCommand
 
 
-
-
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('--path', type=str)
