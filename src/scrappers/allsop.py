b26457a7914f1b095de34115fbd1810884faaecd
bhatti013 <wahabbhatti22@gmail.com>
2022-06-04 20:27:54 +0500

    refactor work

--- src/scrappers/allsop.py
+++ src/scrappers/allsop.py
@@ -1,40 +1,40 @@
 import requests
 from scrappers.traceback import get_traceback
 from extraction_services.models import HouseAuction, ErrorReport
-
+import dateparser
+from price_parser import parse_price
 
 class AllSop:
     DOMAIN = 'https://auctions.allsop.co.uk'
-    URL = 'https://auctions.allsop.co.uk/api/search?future_auctions=on&page=1&size=500&sortOrder=Max%20Price'
+    URL = 'https://auctions.allsop.co.uk/api/search?future_auctions=on&page=1&size=5000&sortOrder=Max%20Price'
 
     def __init__(self):
         pass
 
-    def connect_to(self, url, payload={}):  # fixme {}
+    def connect_to(self, url):  # fixme {}
         headers = {
             'authority': 'auctions.allsop.co.uk',
             'accept': '*/*',
             'accept-language': 'en-US,en;q=0.9',
-            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Linux"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-origin',
             'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36',
             'x-powered-by': 'React 15.7.0',
         }
 
-        re = requests.get(url, headers=headers, data=payload)
-        return re
+        res = requests.get(url, headers=headers)
+        return res
 
-    def prepare_price(self, price):
-        # fixme : none type object error
-        price = price.split('-')[0].replace('£', '').replace('+', '').replace(",", "")
-        if 'M' in price:
-            return float(price.replace("M", '')) * 1000000
-        else:
-            return float(price) * 100000  # fixme : why multply with fix value
+    def currency_iso_name(self, currency_symbol):
+        symbols = {
+            "£": "GBP",
+            "$": "USD",
+        }
+        try:
+            return symbols[currency_symbol]
+        except:
+            raise Exception(f"Currency symbol \"{currency_symbol}\" not matching with available ones.")
+
+    def prepare_price(self,price):
+        pass
 
     def parser(self, data):
 
@@ -44,18 +44,17 @@ class AllSop:
                 url = f"https://auctions.allsop.co.uk/api/lot/reference/{reference_no}?react"
                 res = self.connect_to(url)
                 details = res.json()
-                price_str = details['version']['lot']['guide_price_text']
-                price = self.prepare_price(price_str)
-                auction_date = details['version']['allsop_auction']['allsop_auctiondate']
-                auction_hours = auction_date.split('T')[1]
-                auc_hours = ":".join([auction_hours.split(":")[0], auction_hours.split(":")[1]])
+                price_obj = parse_price(details['version']['lot']['guide_price_text'].replace('M','000000'))
+                price = price_obj.amount_float
+                currency = self.currency_iso_name(price_obj.currency)
+                auction_date = dateparser.parse(details['version']['allsop_auction']['allsop_auctiondate'])
                 features = "\n".join([value['value'] for value in details["version"]['features']])
                 image_id = details["version"]['images'][0]['file_id']
                 image_url = f"https://ams-auctions-production-storage.s3.eu-west-2.amazonaws.com/image_cache/{image_id}---auto--.jpg"
                 data_hash = {
-                    # "_id": details["version"]["allsop_auctionid"],
+                    "_id": details["version"]["allsop_auctionid"],
                     "price": price,
-                    "currency_type":"$", #TODO: Add currency type
+                    "currency_type":currency,
                     "picture_link": image_url,
                     "property_description": features,
                     "property_link": res.url,
@@ -64,8 +63,7 @@ class AllSop:
                     "number_of_bedrooms": details["version"]['allsop_property']['allsop_bedrooms'],
                     "property_type": details['version']['tenancy_type'],
                     "tenure": details['version']['allsop_propertytenure'],
-                    "auction_datetime": auction_date, #TODO : combine date and hour
-                    # "auction_hour": auc_hours,  combine it with auction_date_time
+                    "auction_datetime": auction_date,
                     "auction_venue": details['version']['allsop_auction']['allsop_venue'],
                     "source": "auctionhouse.co.uk"
                 }
@@ -84,10 +82,8 @@ class AllSop:
 
     def scraper(self):
         response = self.connect_to(self.URL)
-        item = []
-        items = self.parser(response.json())
-        # insertion needs to be done yet
-
+        self.parser(response.json())
+        
 
 def run():
     AllSop().scraper()
