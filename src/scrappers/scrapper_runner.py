0529f0c54fa83a052530bd93a1c7c5f5ef67b4cf
Faizan Alhassan <faizanalhassan@gmail.com>
2022-07-17 23:29:33 +0500

    error in logger

--- src/scrappers/scrapper_runner.py
+++ src/scrappers/scrapper_runner.py
@@ -18,14 +18,14 @@ def run():
         module_names.remove("__pycache__")
     for name in module_names:
         # logging.info("Running :", name)
-        logging.info("Running :", name)
+        logging.info(f"Running :{name}")
         try:
             module = importlib.import_module(f"scrappers.{name[:-3]}")
             if hasattr(module, 'run'):
                 module.run()
         except BaseException as be:
             # logging.error("error in :", name)
-            logging.error("error in :", name)
+            logging.error(f"error in :{name}")
             save_error_report(be, name)
             # _traceback = get_traceback()
             # if error_report := ErrorReport.objects.filter(trace_back=_traceback).first():
