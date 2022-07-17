13a83f437b6777ce668c14a20c87a661cfd333e0
Faizan Alhassan <faizanalhassan@gmail.com>
2022-07-17 23:20:44 +0500

    logger try

--- src/scrappers/scrapper_runner.py
+++ src/scrappers/scrapper_runner.py
@@ -4,11 +4,12 @@ import os
 import importlib
 import logging
 # logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(format="%(name)s :: %(levelname)s :: %(message)s", level=logging.DEBUG)
 
 
 def run():
     # logging.info("Starting....")
-    print("Starting....")
+    logging.info("Starting....")
     module_names = os.listdir("scrappers")
     module_names.remove("__init__.py")
     module_names.remove("scrapper_runner.py")
@@ -17,14 +18,14 @@ def run():
         module_names.remove("__pycache__")
     for name in module_names:
         # logging.info("Running :", name)
-        print("Running :", name)
+        logging.info("Running :", name)
         try:
             module = importlib.import_module(f"scrappers.{name[:-3]}")
             if hasattr(module, 'run'):
                 module.run()
         except BaseException as be:
             # logging.error("error in :", name)
-            print("error in :", name)
+            logging.error("error in :", name)
             save_error_report(be, name)
             # _traceback = get_traceback()
             # if error_report := ErrorReport.objects.filter(trace_back=_traceback).first():
